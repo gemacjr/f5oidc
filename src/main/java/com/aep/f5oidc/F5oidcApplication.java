d9d191f0de046adb2d2a250a2da28d813e590cc3
Ed <edmccormic@gmail.com>
2019-12-06 10:49:17 -0500

    init

---
+++ src/main/java/com/aep/f5oidc/F5oidcApplication.java
@@ -0,0 +1,13 @@
+package com.aep.f5oidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F5oidcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F5oidcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aep/f5oidc/F5oidcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aep.f5oidc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class F5oidcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
